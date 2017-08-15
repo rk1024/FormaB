1850629c6c172b6b7de13ccfd001783f3e7f0c4e
rookie1024 <rookie1286@gmail.com>
2017-08-15 11:47:51 -0700

    Added better printing functions to class FormaAST.

--- src/ast/astBase.hpp
+++ src/ast/astBase.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
 namespace frma {
 class FormaAST {
@@ -13,5 +15,20 @@ public:
   virtual void print(std::ostream &) const {}
 
   inline bool rooted() const { return m_rooted; }
+
+  std::string toString() const {
+    std::ostringstream oss;
+    print(oss);
+    return oss.str();
+  }
 };
+
+inline std::ostream &operator<<(std::ostream &os, const FormaAST &ast) {
+  ast.print(os);
+  return os;
+}
+inline std::ostream &operator<<(std::ostream &os, const FormaAST *ast) {
+  ast->print(os);
+  return os;
+}
 }

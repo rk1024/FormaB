446644452c452bf7592d05339bd572c68ce1725d
rookie1024 <rookie1286@gmail.com>
2017-08-21 16:36:22 -0700

    Updated submodules, added re2c.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+OLDWD=$PWD
+cd "$(dirname $0)"
+
 git submodule update --init --recursive --remote --force
 git config --local core.hooksPath scripts/git-hooks
+scripts/setup-re2c.sh
+
+cd $OLDWD

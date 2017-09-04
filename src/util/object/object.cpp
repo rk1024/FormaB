10dd0d4d3187c5285723fabd52b3f2ab4e72a984
rookie1024 <rookie1286@gmail.com>
2017-09-04 02:15:51 -0700

    Fixed some minor issues with intrusive refcounting

--- src/util/object/object.cpp
+++ src/util/object/object.cpp
@@ -5,17 +5,17 @@
 
 namespace fun {
 FObject::~FObject() {
-  assert(m_tracker->m_tracked);
-  if (m_tracker->m_tracked != FRefTracker::COUNT_DESTROYING) {
+  assert(tracker()->m_tracked);
+  if (tracker()->m_tracked != FRefTracker::COUNT_DESTROYING) {
     std::cerr << "\e[1;38;5;3mwarning: \e[39mFObject not destroyed by its "
                  "tracker; detaching"
               << std::endl;
 
-    m_tracker->m_target = nullptr;
+    tracker()->m_target = nullptr;
   }
 }
 
-void FObject::acquire() { m_tracker->trackAcquire(); }
-void FObject::release() { m_tracker->trackRelease(); }
-void FObject::reset() { m_tracker->trackReset(); }
+void FObject::acquire() { tracker()->trackAcquire(); }
+void FObject::release() { tracker()->trackRelease(); }
+void FObject::reset() { tracker()->trackReset(); }
 }
